f98281bf2a0c4b69018d9c79928304929c8f5966
Bala Krishnan M <bala13.bkm@gmail.com>
2018-09-05 20:53:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import psycopg2
+
+conn = psycopg2.connect(host="localhost",database="postgres", user="postgres", password="123456")
+cur = conn.cursor()
+
+print('PostgreSQL database version:')
+cur.execute('CREATE TABLE user3 (id INT, name VARCHAR(255))')
+conn.commit()
